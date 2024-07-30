6ce7e1af058c2eb038c5f29d15b807a31d314533
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-30 16:15:53 +0700

    Setup ListNode

--- untitled/src/solutions/Solution.java
+++ untitled/src/solutions/Solution.java
@@ -1,4 +1,19 @@
 package solutions;
 
 public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {this.val = val;}
+        ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+    }
+
+    public static int getDecimalValue(ListNode head) {
+
+    }
 }
